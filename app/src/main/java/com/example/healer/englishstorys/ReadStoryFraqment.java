2543df3173dd65a01b3c74b5716bddab441aa580
downloadofvinh <downloadofvinh@gmail.com>
2017-03-13 18:29:15 +0700

    giao dien readstory

--- app/src/main/java/com/example/healer/englishstorys/ReadStoryFraqment.java
+++ app/src/main/java/com/example/healer/englishstorys/ReadStoryFraqment.java
@@ -1,43 +1,40 @@
 package com.example.healer.englishstorys;
 
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.support.design.widget.BottomSheetBehavior;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment;
+import android.widget.RelativeLayout;
+
 
 /**
  * Created by Healer on 21-Feb-17.
  */
 
-public class ReadStoryFraqment extends Fragment implements View.OnTouchListener{
-    private BottomSheetBehavior audioBar;
+public class ReadStoryFraqment extends Fragment{
+    BottomSheetBehavior bottomSheetBehavior;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle BundlesavedInstanceState){
         View rootView = inflater.inflate(R.layout.readstory_tab,container,false);
-        View BottomSheet_AudioBar = rootView.findViewById(R.id.bottomsheet_audio);
-        audioBar  = BottomSheetBehavior.from(BottomSheet_AudioBar);
+        View bottomSheet = rootView.findViewById(R.id.audioBar);
+        bottomSheetBehavior = BottomSheetBehavior.from((View) bottomSheet);
+        bottomSheetBehavior.setPeekHeight(0);
+        RelativeLayout layout_story = (RelativeLayout) rootView.findViewById(R.id.layout_story);
+
+        layout_story.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(bottomSheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED) {
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                }
+                else {
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                }
+                Log.d("vinh1","vinh1");
+            }
+        });
         return rootView;
-    }
-    @Override
-    public boolean onTouch(View view, MotionEvent motionEvent) {
-        int action = motionEvent.getAction();
-        if(action == motionEvent.ACTION_DOWN){
-            Log.d("vinh","vinh");
-            if(audioBar.getState() != BottomSheetBehavior.STATE_EXPANDED) {
-                audioBar.setState(BottomSheetBehavior.STATE_EXPANDED);
-            }
-            else {
-                audioBar.setState(BottomSheetBehavior.STATE_COLLAPSED);
-            }
-            return true;
-        }
-        if(action == motionEvent.ACTION_UP){
-            Log.d("vinh1","vinh1");
-        }
-        return false;
     }
 }
